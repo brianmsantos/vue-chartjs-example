9187fe726e679b53b49898a0c4a480bb282b52f4
Brian Santos <brianmsantos81@gmail.com>
2019-09-16 14:24:50 -0700

    vue chartjs component complete for line chart

---
+++ src/components/MonthlySalesChart.vue
@@ -0,0 +1,91 @@
+<template>
+  <div>
+    <div class="field is-grouped">
+      <div class="control">
+        <label class="checkbox">
+          <input type="checkbox" :value="2018" v-model="selectedYears">
+          2018 Sales
+        </label>
+      </div>
+      <div class="control">
+        <label class="checkbox">
+          <input type="checkbox" :value="2017" v-model="selectedYears">
+          2017 Sales
+        </label>
+      </div>
+    </div>
+
+    <line-chart
+      :width="500"
+      :height="300"
+      :labels="['Jan', 'Feb', 'Mar', 'Apr', 'May']"
+      :datasets="displayedDatasets"
+      :options="$options.options"
+    ></line-chart>
+  </div>
+</template>
+
+<script>
+import numeral from 'numeral';
+import LineChart from './LineChart';
+
+// Key the datasets by year for easy access.
+const datasets = {
+  2018: {
+    label: '2018 Sales',
+    borderColor: 'rgba(50, 115, 220, 0.5)',
+    backgroundColor: 'rgba(50, 115, 220, 0.1)',
+    data: [300, 700, 450, 750, 450]
+  },
+  2017: {
+    label: '2017 Sales',
+    borderColor: 'rgba(255, 56, 96, 0.5)',
+    backgroundColor: 'rgba(255, 56, 96, 0.1)',
+    data: [600, 550, 750, 250, 700]
+  }
+};
+
+const options = {
+  scales: {
+    yAxes: [{
+      ticks: {
+        beginAtZero: true,
+        callback: value => numeral(value).format('$0,0')
+      }
+    }]
+  },
+  tooltips: {
+    mode: 'index',
+    callbacks: {
+      label(tooltipItem, data) {
+        const label = data.datasets[tooltipItem.datasetIndex].label;
+        const value = numeral(tooltipItem.yLabel).format('$0,0');
+
+        return `${label}: ${value}`;
+      }
+    }
+  }
+};
+
+export default {
+  name: 'monthly-sales-chart',
+  datasets,
+  options,
+  components: {
+    LineChart
+  },
+  data() {
+    return {
+      // Select both years by default.
+      selectedYears: [2018, 2017]
+    };
+  },
+  computed: {
+    // The datasets to display.
+    displayedDatasets() {
+      return this.selectedYears.map(year => datasets[year]);
+    }
+  }
+}
+
+</script>

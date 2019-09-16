a9743a9165d86df7941729488a5c070444ffca47
Brian Santos <brianmsantos81@gmail.com>
2019-09-16 15:03:11 -0700

    started adding in our own NPS bar chart

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,7 @@
 <template>
   <div class="box">
+    <h2 class="title is-5">NPS Bar Chart</h2>
+    <nps-bar-chart></nps-bar-chart>
     <h2 class="title is-5">Monthly Sales Chart</h2>
     <monthly-sales-chart></monthly-sales-chart>
   </div>
@@ -7,10 +9,12 @@
 
 <script>
 import MonthlySalesChart from './components/MonthlySalesChart';
+import NPSBarChart from './components/NPSBarChart'
 
 export default {
   name: 'app',
   components: {
+    NPSBarChart,
     MonthlySalesChart
   }
 }
---
+++ src/components/NPSBarChart.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    Promoters
+    <br>
+    Passives
+    <br>
+    Detractors
+  </div>
+
+<!-- This is where the bar chart goes. -->
+</template>
+
+<script>
+import BarChart from './BarChart'
+// Datasets
+const datasets = {
+  // label: ,
+  // borderColor: ,
+  // backgroundColor: ,
+  data: [4, 3, 3, 5, 3, 1, 0, 7, 10, 99, 59]
+};
+
+const options = {
+  scales: {
+    xAxes: [{
+      ticks: {
+        beginAtZero: true,
+      }
+    }]
+  }
+}
+
+export default {
+  name: 'nps-bar-chart', 
+  datasets, 
+}
+
+</script>
